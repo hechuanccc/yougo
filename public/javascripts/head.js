c4fd669436a3f2ef3b780228fe120ece3725d549
collin2 <corlin@unnotech.com>
2018-06-19 22:16:45 +0800

    完成cookie讲解

---
+++ public/javascripts/head.js
@@ -0,0 +1,77 @@
+
+
+var oGameMenuBox = document.getElementById('gameMenu')
+
+ajax({
+	url: 'http://b9088.com/member/gamecategory/?mode=tree',
+	success: function (resData) {
+		var data
+		try {
+			data = JSON.parse(resData)
+			console.log('哈哈哈哈')
+			console.log(data)
+			handleGameCategory(data)
+		} catch(e) {
+			// statements
+			console.log(e);
+		}
+	},
+	error: function (status) {
+		console.log('error')
+		console.log(status)
+	}
+})
+
+
+function handleGameCategory (arr) {
+	if (!arr || !arr.length) return
+	for(var i = 0; i < arr.length; i++) {
+		var oLi = document.createElement('li')
+
+		oLi.className = 'fl'
+
+		var str = ''
+
+		for(var j = 0; j < arr[i].provider.length; j++) {
+			str += '<div class="item fl">\
+						<img src="'+ arr[i].provider[j].icon +'" alt="">\
+						<p>'+ arr[i].provider[j].name + arr[i].provider[j].category_type.slice(0, 2) +'</p>\
+					</div>'
+		}
+
+		oLi.innerHTML = ''+ arr[i].name +'\
+						<div class="drop-down">\
+							<div class="game-category m-c clearfix">\
+								'+ str +'\
+							</div>\
+						</div>\
+\
+						<div class="caret"></div>'
+
+		oGameMenuBox.appendChild(oLi)
+	}
+
+
+	for(var i = 1; i < oGameMenuBox.children.length; i++) {
+		oGameMenuBox.children[i].onmouseenter = function () {
+			for(var i = 1; i < oGameMenuBox.children.length; i++) {
+				oGameMenuBox.children[i].children[0].style.display = 'none'
+			}
+			this.children[0].style.display = 'block'
+		}
+
+		oGameMenuBox.children[i].onmouseleave = function () {
+			this.children[0].style.display = 'none'
+		}
+
+	}
+
+	var aDrops = document.getElementsByClassName('drop-down')
+	var clientW = document.documentElement.clientWidth || document.body.clientWidth
+
+
+	for(var i = 0; i < aDrops.length; i++) {
+		aDrops[i].style.width = clientW + 'px'
+		aDrops[i].style.left = - aDrops[i].parentNode.offsetLeft + 'px'
+	}
+}
